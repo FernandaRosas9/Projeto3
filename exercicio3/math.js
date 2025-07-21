6239fd110d6ee5e9a4cc55bdc91f21095c5549f8
gabrielramalhao <gabriel123.ramalhao@gmail.com>
2025-07-21 12:25:45 -0300

    feat: exercicio3 added

---
+++ exercicio3/math.js
@@ -0,0 +1,20 @@
+// Exercício A
+const average = (num1, num2, num3) => {
+    return (num1 + num2 + num3) / 3;
+}
+
+console.log(average(15, 25, 38));
+
+// Exercício B
+const convertToString = (str) => {
+    return Number(str);
+}
+
+console.log(convertToString("123.45"));
+
+// Exercício C
+const roundNumber = (number) => {
+    return Math.round(number / 2);
+}
+
+console.log(roundNumber(9.7));
